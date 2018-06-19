30c05dd7b73b0b4844d3ae52e3e3a26e58679304
Cygni94 <ccoelho.me@gmail.com>
2018-06-19 14:33:30 -0300

    "api" connected!

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,34 @@ import React, { Component } from "react";
 import VeiculoInfo from "./components/veiculo_info";
 import "./App.css";
 
-const Moto = {
-  tipo: 'Moto',
-  properties: {
-    fotoURL: 'string',
-    fabricante:  'string',
-    modelo: 'string',
-    quilometragem: {type: 'int', default: 0},
+const Motos = {
+  Factor: {
+    propriedades: {
+      fotoURL: 'assets/img/factor.jpg',
+      fabricante:  'Yamaha',
+      modelo: 'Factor 125i ED'
+    }
+  },
+  Fazer: {
+    propriedades: {
+    fotoURL: 'https://www.yamaha-motor.com.br/fazer250abs/assets/img/comparativo/fazer.png',
+    fabricante:  'Yamaha',
+    modelo: 'Fazer 250 ABS'
   }
+}
 }
 
 class App extends Component {
 
-  _renderObject(){
-		return Object.entries(ObjectTest)
+	_renderObject(){
+		return Object.entries(Motos).map(([key, value], i) => {
+			return (
+				<div key={key}>
+					Modelo: {value.propriedades.modelo}
+					Fabricante: {value.propriedades.fabricante}
+				</div>
+			)
+		})
 	}
 
  render() {
