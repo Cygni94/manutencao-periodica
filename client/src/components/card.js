46cb93d8b289dca072ddb17641f390e2d679d929
Cygni94 <ccoelho.me@gmail.com>
2018-06-19 17:55:00 -0300

    190620181754

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -4,33 +4,33 @@ const Card = ({ veiculo }) => {
   return (
     <div className="container card">
       <div className="card__header">
-      <figure class="photo">
-        <img class="img-fluid photo__img" src="assets/img/factor.jpg" />
-        <figcaption class="photo__caption">Yamaha Factor 2017</figcaption>
-      </figure>
+        <figure class="photo">
+          <img class="img-fluid photo__img" src="assets/img/factor.jpg" />
+          <figcaption class="photo__caption">Yamaha Factor 2017</figcaption>
+        </figure>
       </div>
       <div className="card__body">
-<h1>card body</h1>
+        <h1>card body</h1>
       </div>
       <div className="card__footer">
-      <div className="row">
-        <ul class="nav justify-content-center">
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Active
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Active
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Active
-            </a>
-          </li>
-        </ul>
+        <div className="row">
+          <ul class="nav justify-content-center">
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Active
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Active
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Active
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
