99c4093d5e3ae6b65755225e725d2d3929c685f8
Cygni94 <ccoelho.me@gmail.com>
2018-06-19 23:25:32 -0300

    melhorando

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,40 +1,45 @@
 import React from "react";
 
 const Card = ({ veiculo }) => {
-  return (
-    <div className="container card">
-      <div className="card__header">
-        <figure class="photo">
-          <img class="img-fluid photo__img" src="assets/img/factor.jpg" />
-          <figcaption class="photo__caption">Yamaha Factor 2017</figcaption>
-        </figure>
-      </div>
-      <div className="card__body">
-        <h1>card body</h1>
-      </div>
-      <div className="card__footer">
-        <div className="row">
-          <ul class="nav justify-content-center">
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Active
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Active
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Active
-              </a>
-            </li>
-          </ul>
+    return (
+        <div className="container card">
+            <div className="card__header">
+                <figure className="photo">
+                    <img
+                        className="img-fluid photo__img"
+                        src="assets/img/factor.jpg"
+                    />
+                    <figcaption className="photo__caption">
+                        Yamaha Factor 2017
+                    </figcaption>
+                </figure>
+            </div>
+            <div className="card__body">
+                <h1>card body</h1>
+            </div>
+            <div className="card__footer">
+                <div className="row">
+                    <ul className="nav justify-content-center">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                Active
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                Active
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                Active
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Card;
