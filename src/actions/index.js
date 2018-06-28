02977c073181a21cc38919d884ff52c4c62fb1f7
Cygni94 <ccoelho.me@gmail.com>
2018-06-27 23:46:30 -0300

    implantando redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,23 @@
 export const FETCH_VEICULOS = "FETCH_VEICULOS";
 
-export function fetchVeiculos() {
-    const request = fetch(`http://localhost:3666/data`)
-        .then(result => result.json())
-        .then(veiculos =>
-            this.setState({
-                veiculos: [
-                    {
-                        id: veiculos.id,
-                        tipo: veiculos.tipo,
-                        fabricante: veiculos.fabricante,
-                        modelo: veiculos.modelo,
-                        fotoURL: veiculos.fotoURL,
-                    },
-                ],
-            })
-        );
+const ROOT_URL = "http://localhost:3666/data";
+
+export function fetchVeiculos(state = {}, action) {
+    const request = fetch(`ROOT_URL`);
+    // .then(result => result.json())
+    // .then(veiculos =>
+    //     this.setState({
+    //         veiculos: [
+    //             {
+    //                 id: veiculos.id,
+    //                 tipo: veiculos.tipo,
+    //                 fabricante: veiculos.fabricante,
+    //                 modelo: veiculos.modelo,
+    //                 fotoURL: veiculos.fotoURL,
+    //             },
+    //         ],
+    //     })
+    // );
 
     return {
         type: FETCH_VEICULOS,
--- src/reducers/reducer_veiculos.js
+++ src/reducers/reducer_veiculos.js
@@ -3,8 +3,9 @@ import { FETCH_VEICULOS } from "../actions/index";
 export default function(state = {}, action) {
     switch (action.type) {
         case FETCH_VEICULOS:
+            return state;
+        //return [action.payload.data, ...state];
+        default:
             console.log(action.payload.data);
-            return [action.payload.data, ...state];
     }
-    return state;
 }
