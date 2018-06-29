b8ab0552101a1d78c85c39834089ea29213e0069
Cygni94 <ccoelho.me@gmail.com>
2018-06-29 00:06:31 -0300

    oks

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,13 @@
 export const FETCH_VEICULOS = "FETCH_VEICULOS";
+export const FETCH_VEICULO = "FETCH_VEICULO";
+export const CREATE_VEICULO = "CREATE_VEICULO";
+export const DELETE_VEICULO = "DELETE_VEICULO";
+
+const ROOT_URL =
+    "https://my-json-server.typicode.com/coelhojs/manutencao-periodica/db";
 
 export function fetchVeiculos(state = {}, action) {
-    const request = fetch(
-        "https://my-json-server.typicode.com/coelhojs/manutencao-periodica/db"
-    ).then(function(response) {
+    const request = fetch(`${ROOT_URL}`).then(function(response) {
         return response.json();
     });
     return {
--- src/reducers/reducer_veiculos.js
+++ src/reducers/reducer_veiculos.js
@@ -1,10 +1,21 @@
-import { FETCH_VEICULOS } from "../actions/index";
+import _ from "lodash";
+import {
+    FETCH_VEICULOS,
+    FETCH_VEICULO,
+    CREATE_VEICULO,
+    DELETE_VEICULO,
+} from "../actions/index";
 
 export default function(state = {}, action) {
     switch (action.type) {
         case FETCH_VEICULOS:
-            console.log(action.payload.data);
             return action.payload.data;
+        case FETCH_VEICULO:
+            return { ...state, [action.payload.data.id]: action.payload.data };
+        case CREATE_VEICULO:
+            return { ...state, [action.payload.data.id]: action.payload.data };
+        case DELETE_VEICULO:
+            return _.omit(state, action.payload.data.id);
         default:
             return state;
     }
